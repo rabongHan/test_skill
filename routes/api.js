e1f5944d66e2d659f97c4ecd6b670edeec8c4125
rabongHan <recognize0117@gmail.com>
2021-08-21 20:57:51 +0900

    dateSecond added newly

--- routes/api.js
+++ routes/api.js
@@ -17,9 +17,10 @@ apiRouter.get('/keyboard', function(req, res) {
 apiRouter.post('/meal', async function(req, res) {
    //  const result = await example();
    const dateInfo = req.body.userRequest.utterance; 
+   const dateSecond = dateInfo.substring(9,11);
 
    const meal = await school.getMeal({default: '이 날은 급식이 없습니다.'});
-   const printing = meal[dateInfo]
+   const printing = meal[dateSecond]
    console.log(req.body);
    
    const responseBody = {
@@ -28,7 +29,7 @@ apiRouter.post('/meal', async function(req, res) {
        outputs: [
          {
            simpleText: {
-             text: `${meal.month}월 ${dateInfo}일 급식정보 \n` + printing
+             text: `${meal.month}월 ${dateSecond}일 급식정보 \n` + printing
            }
          }
        ]
