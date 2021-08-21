04e9a55e34889217d4ca1ddba6668750077f5c46
rabongHan <recognize0117@gmail.com>
2021-08-21 22:13:47 +0900

    0821 monthlymeal new post add

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,7 @@ const School = require('school-kr')
 const school = new School()
 
 school.init(School.Type.HIGH, School.Region.GYEONGGI, 'J100005286')
-// const example = async function() {
-//   const meal = await school.getMeal();
-//   return(meal)
-// }
+
 
 apiRouter.get('/keyboard', function(req, res) {
    const data = {'type' : 'text'}
@@ -15,12 +12,10 @@ apiRouter.get('/keyboard', function(req, res) {
 });
 
 apiRouter.post('/meal', async function(req, res) {
-   //  const result = await example();
    const dateInfo = req.body.userRequest.utterance; 
-   const dateSecond = dateInfo.substring(9,11);
-
+   
    const meal = await school.getMeal({default: '이 날은 급식이 없습니다.'});
-   const printing = meal[dateSecond]
+   const printing = meal[dateInfo]
    console.log(req.body);
    
    const responseBody = {
@@ -29,14 +24,37 @@ apiRouter.post('/meal', async function(req, res) {
        outputs: [
          {
            simpleText: {
-             text: `${meal.month}월 ${dateSecond}일 급식정보 \n` + printing
+             text: `${meal.month}월 ${dateInfo}일 급식정보 \n` + printing
            }
          }
        ]
      }
    };
-
    res.status(200).send(responseBody);
  });
+ 
+ apiRouter.post('/monthlymeal', async function(req,res) {
+   const datetime = req.body.action.params;
+   const pre_result = datetime.get("datetime");
+   const monthlymeal_result = pre_result.substring(18,20);
+
+   const meal2 = await school.getMeal({default: '이 날은 급식이 없습니다.'});
+   const monthly_printing = meal[monthlymeal_result];
+   console.log(req.body);
+
+   const responseBody = {
+     version: "2.0",
+     template: {
+       outputs: {
+         simpleText: {
+           text: `${meal2.month}월 ${monthlymeal_result}일 급식정보 \n` + monthly_printing
+         }
+       }
+     }
+   };
+   res.status(200).send(responseBody)
+ });
+
+
 module.exports = apiRouter;
 
