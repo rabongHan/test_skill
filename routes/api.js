5139fd82ee1df18c7fa766e8460aa71c36007b98
rabongHan <recognize0117@gmail.com>
2021-08-21 22:50:18 +0900

    0821 delete monthlymeal

--- routes/api.js
+++ routes/api.js
@@ -33,29 +33,5 @@ apiRouter.post('/meal', async function(req, res) {
    res.status(200).send(responseBody);
  });
  
- apiRouter.post('/monthlymeal', async function(req,res) {
-   const monthlydatetime = req.body.action.detailParams.datetime.origin;
-   const monthlymeal_result = monthlydatetime.substring(9,11);
-
-   const meal2 = await school.getMeal({default: '이 날은 급식이 없습니다.'});
-   const monthly_printing = meal2[monthlymeal_result];
-   console.log(req.body);
-
-   const responseBody = {
-     version: "2.0",
-     template: {
-       outputs: [
-         {
-           simpleText: {
-             text: `${meal2.month}월 ${monthlymeal_result}일 급식정보 \n` + monthly_printing
-           }
-         }
-       ] 
-     }
-   };
-   res.status(200).send(responseBody)
- });
-
-
 module.exports = apiRouter;
 
